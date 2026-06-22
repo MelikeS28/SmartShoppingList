43f6ed4e353ed236576251a5cdd668e09bcfe413
Melike Sezgin <mel.sez.19@gmail.com>
2026-06-22 17:08:33 +0300

    Initial Commit

---
+++ SmartShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartShoppingList
+//
+//  Created by Melike on 22.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
